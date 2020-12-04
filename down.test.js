63fb301d956fdc87d77f40537d91cf9583b33a1d
Nworm <1574242600@qq.com>
2020-12-04 19:20:46 +0800

    feat: download test

---
+++ down.test.js
@@ -0,0 +1,14 @@
+const cookies = {
+    'ipb_member_id': 3512590,
+    'ipb_pass_hash': 'cfb712ea2633f9894c5dae23146f78d0',
+    'igneous': '322abe39d'
+} //公共账号   来自https://ex.acg.uy/
+
+const ehApi = require('./exApi').default;
+const exapi = new ehApi(cookies);
+
+it('测试 download', () => {
+    return exapi.downloadGallery(['627844', '39dbc33ad8']).then(statusList => {
+        expect(statusList).not.toBe(false);
+    });
+})
